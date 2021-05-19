af44160b9ab3b091c2a27409d5a16af9edd0e220
meezymeek <hmeek72@gmail.com>
2021-05-19 15:28:45 -0700

    fixed server startup errors. server starts on app.js instead of index.js

---
+++ app.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
+
+//all the required shit---------------------------------------------------------
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+//responses --------------------------------------------------------------------
+const responses = require('./utils/responses');
+
+// user functions---------------------------------------------------------------
+const userJoin = require('./utils/userJoin');
+const userLeave = require('./utils/userKick');
+
+// When the bot boots up -------------------------------------------------------
+bot.on("ready", async () =>{
+	console.log(`${bot.user.username} is online!`);
+
+	//set game -------------------------------------------------------------------
+	bot.user.setActivity("looking at your pictures");
+
+});
+
+//on user add/remove------------------------------------------------------------
+bot.on('guildMemberAdd', member  => {
+	userJoin(bot, member);
+});
+bot.on('guildMemberRemove', member => {
+	userLeave(bot, member);
+});
+
+//message functions-------------------------------------------------------------
+bot.on("message", async message => {
+	//if autor is a bot, ignore
+	if(message.author.bot) return;
+	//if message is DM, ignore
+	if(message.channel.type === "dm") return;
+
+/******************************************************************************/
+	//for testing only------------------------------------------------------------
+	//if(message.guild.id == '[GUILD_ID]') return;
+/******************************************************************************/
+
+//responses and messages -------------------------------------------------------
+		responses(message, bot);
+});
+
+const botToken = require('./bottoken.json');
+bot.login(botToken.token);
